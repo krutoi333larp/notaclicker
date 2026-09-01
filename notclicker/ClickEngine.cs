4ed895e290ec45e2c76117cdd4fe452d4f74e3c3
krutoi333larp <butylka953@gmail.com>
2026-09-01 17:13:48 +1000

    my first 300 lines of code

---
+++ notclicker/ClickEngine.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace notclicker
+{
+    public class ClickEngine
+    {
+        //я знаю что мог написать это через Keyboardhook но узнал я про эту библиотеку только когда решил писать хуки клавиш
+        //мной было принято решение оставить winapi метод в пользу скорости работы т.к хуки клавиатуры писать сложно а простой кликер лёгок в написании
+        //импорт системмной дллки
+        [DllImport("user32.dll")]
+        //состояния мыши
+        private static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint dwData, int dwExtraInfo);
+        private const uint MOUSEEVENTF_LEFTDOWN = 0x0002; //нажал
+        private const uint MOUSEEVENTF_LEFTUP = 0x0004; //отпустил
+
+        //проверка на кликанье
+        public bool isclicking = false;
+        //стандартная задержка между кликами
+        private int clickdelay = 10;
+
+        //защита от краша компа
+        public int ClickDelay
+        {
+            get {  return clickdelay; } //возвращает нам clickdelay при попытке получить переменную из другой части кода
+            set
+            {
+                if (value < 10) //проверка на задержку менее 10 мс
+                {
+                    clickdelay = 10;
+                }
+                else //если задержка приемлима то ставится то что поставил пользователь
+                {
+                    clickdelay = value;
+                }
+            }
+
+        }
+        public void Start()
+        {
+            if (!isclicking) //проверка кликает ли сейчас программа
+            {
+                isclicking = true; //установка состояния кликания в true
+                _ = ClickAsync(); //включение самого кликера
+
+            }
+            
+
+        }
+        public void Stop() //метод остановки 
+        {
+            isclicking = false;
+        }
+
+
+        private async Task ClickAsync() //метод самого кликера
+        {
+            while (isclicking) //цикл при включенном isclicking
+            {
+                mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0); //нажатие
+                mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0); //отжатие
+                
+                //ассинхронная пауза
+                await Task.Delay(clickdelay);
+
+            }
+
+
+
+        }
+    }
+}
---
+++ notclicker/MainWindow.xaml.cs
@@ -0,0 +1,122 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using KeyboardHook;
+using KeyboardHook.Enums;
+using KeyboardHook.Interfaces;
+//скрипт предназначен для работы с окном а так же хука клавиш
+namespace notclicker
+{
+    
+    public partial class MainWindow : Window
+    {
+        
+        private ClickEngine engine = new ClickEngine(); //движок кликов
+        private readonly IKeyboardHook _hook = KeyboardHookFactory.Create(); //хук клавы
+        private KeyboardKey _bind; //кнопка бинда
+        private string bindtype = "hold"; //тип бинда
+        private bool isalistening = false; //проверка на считывание бинда
+        private bool pressed = false; //проверка на нажатие для тогл бинда
+        public MainWindow()
+        {
+            
+            InitializeComponent();
+            _hook.KeyDown += onglobalkeydown;//подписка на нажатие клавиш
+            _hook.KeyUp += onglobalkeyup;//подписка на отпускание клавиш
+
+        }
+        private void buttonbindclick(object sender, RoutedEventArgs e)
+        {
+            if (!isalistening)
+            {
+                isalistening = true; //
+                BindBtn.Content = "press key"; //меняет текст кнопки
+
+            }    
+
+
+
+        }
+        private void onglobalkeydown(KeyboardKey key) //key кнопка которая передалась после нажатия
+        {
+            
+
+            if (key != _bind) return; //если нажали не нашу кнопку иди нахуй
+
+            else if (key == _bind) //если короче клавиша это бинд 
+            {
+                if (bindtype == "hold") //проверка на тип
+                {
+                    if (engine.isclicking == false) //проверка на кликанье(о да проверка в проверке в проверке)
+                    {
+                        engine.Start();//поехали
+                    }
+                }
+            }
+
+        }
+        private void onglobalkeyup(KeyboardKey key) //отжатие клавиши
+        {
+            if (isalistening)
+            {
+                _bind = key; //ставит бинд в нажатую кнопку
+                isalistening = false; //меняет слушание кнопки на false
+                Dispatcher.Invoke(() => //говорит кнопке поменять свой текст
+                {
+                    BindBtn.Content = $"Бинд: {key}";
+                });
+            }
+            if (key != _bind) return; //если нажали не нашу кнопку иди нахуй
+
+            if (key  == _bind) //проверка что ртпущенная кнопка это бинд
+            {
+                if (bindtype == "hold" || engine.isclicking == true) //проверка на то что у на сработает холд
+                {
+                    engine.Stop(); //выключение
+                }
+                else if (bindtype == "toggle") //альтернативный случай с тоглом
+                {
+                    pressed = !pressed; //изменение состояния нажатости кнопки
+                    if (pressed) //проверка на включенность бинда
+                    {
+                        engine.Start(); //поехали
+                    }
+                    else //альтернативный случай
+                    { 
+                        engine.Stop(); //выключение
+                    }
+                }
+
+            }
+
+
+
+        }
+        private void RadioButton_Checked(object sender, RoutedEventArgs e) //переключатель
+        {
+            if (sender is RadioButton radioButton) //проверка на то что отправитель сигнала кнопка
+            {
+                bindtype = radioButton.Content.ToString().ToLower(); //перевод в выбранный режим
+            }    
+
+        }
+        private void InputDelay_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (engine == null || InputDelay == null) return; //проверка на существование движка и делея
+            
+            if (int.TryParse(InputDelay.Text, out int newDelay)) //перевод делея в int
+            {
+                engine.ClickDelay = newDelay; //установка нового делея
+
+            }
+        }
+        
+    }
+}
